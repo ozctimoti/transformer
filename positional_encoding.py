a9249fb0fda944ca69d7f2f40a4e4c0e46c45514
timoteosonurozcelik <macbook@Timoteos-Macbook.local>
2021-12-23 21:34:54 +0300

    updated considering the cases where batch is located in the first location of embedding such as image classification

--- positional_encoding.py
+++ positional_encoding.py
@@ -9,6 +9,7 @@ class PositionalEncoding(nn.Module):
     def __init__(self,
                  emb_size: int,
                  dropout: float,
+                 batch_first: bool = False,
                  maxlen: int = 5000):
         super(PositionalEncoding, self).__init__()
         den = torch.exp(- torch.arange(0, emb_size, 2) * math.log(10000) / emb_size)
@@ -16,10 +17,19 @@ class PositionalEncoding(nn.Module):
         pos_embedding = torch.zeros((maxlen, emb_size))
         pos_embedding[:, 0::2] = torch.sin(pos * den)
         pos_embedding[:, 1::2] = torch.cos(pos * den)
-        pos_embedding = pos_embedding.unsqueeze(-2)
+        if batch_first:
+            pos_embedding = pos_embedding.unsqueeze(0)
+        else:
+            pos_embedding = pos_embedding.unsqueeze(-2)
 
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('pos_embedding', pos_embedding)
 
+        self.batch_first = batch_first
+
     def forward(self, embedding: Tensor):
-        return self.dropout(embedding + self.pos_embedding[:embedding.size(0), :])
+        if not self.batch_first:
+            return self.dropout(embedding + self.pos_embedding[:embedding.size(0), :])
+        else:
+            return self.dropout(embedding + self.pos_embedding[:, :embedding.size(1)])
+
